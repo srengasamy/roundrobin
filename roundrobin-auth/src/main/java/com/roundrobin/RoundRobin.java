fc20178c6ed019a2cfc6ac8c204b6ced2f8c8322
Rengasamy, Sureshkumar <sureshkumar.rengasamy@verizon.com>
2016-05-25 00:23:02 -0700

    Added auth

---
+++ roundrobin-auth/src/main/java/com/roundrobin/RoundRobin.java
@@ -0,0 +1,14 @@
+package com.roundrobin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rengasu on 5/24/16.
+ */
+@SpringBootApplication
+public class RoundRobin {
+  public static void main(String[] args) {
+    SpringApplication.run(RoundRobin.class, args);
+  }
+}
