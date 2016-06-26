18dd0d0c4c4d00023e917d519af71d593f445f5a
Rengasamy, Sureshkumar <sureshkumar.rengasamy@verizon.com>
2016-06-26 10:32:00 -0700

    Added vault

---
+++ roundrobin-auth/src/main/java/com/roundrobin/auth/config/CustomTokenEnhancer.java
@@ -0,0 +1,25 @@
+package com.roundrobin.auth.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+import com.roundrobin.auth.domain.UserDetail;
+
+@Configuration
+public class CustomTokenEnhancer implements TokenEnhancer {
+  @Override
+  public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+    UserDetail user = (UserDetail) authentication.getPrincipal();
+    final Map<String, Object> additionalInfo = new HashMap<>();
+    additionalInfo.put("userId", user.getUserId());
+    additionalInfo.put("verified", user.isVerified());
+    ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+    return accessToken;
+  }
+}
